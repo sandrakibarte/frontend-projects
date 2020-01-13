ba69abb551b367efb12370821539d7ee0088787e
Sandra Kibarte <55550709+sandrakibarte@users.noreply.github.com>
2020-01-13 09:03:34 +0200

    Create js.js

---
+++ pavyzdys/js.js
@@ -0,0 +1,16 @@
+let searchInput = document.querySelector('#searchInput');
+let searchButton = document.querySelector('#searchButton');
+
+searchButton.addEventListener('click', onButtonClicked);
+
+function onButtonClicked() {
+	alert('ieskau');
+}
+
+function handleResponse(response) {
+	
+}
+
+function renderHtml() {
+
+}
