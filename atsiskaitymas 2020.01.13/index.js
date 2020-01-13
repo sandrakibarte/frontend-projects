f338a992573ceccbf08b7cbd891a2138fd70e03e
Sandra Kibarte <55550709+sandrakibarte@users.noreply.github.com>
2020-01-13 09:30:44 +0200

    Update index.js

--- atsiskaitymas 2020.01.13/index.js
+++ atsiskaitymas 2020.01.13/index.js
@@ -1,4 +1,31 @@
 let btn = document.querySelector('.btn');
-btn.addEventListener("click", function () {
-	alert('labas');
-});
+btn.addEventListener("click", onButtonClicked);
+
+function onButtonClicked() {
+
+	let xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = handleResponse;
+	
+    xmlHttp.open("GET", 'https://www.omdbapi.com/?apikey=e4db3ced&t=titanic');
+    xmlHttp.send();
+    handleResponse(xmlHttp);
+}
+
+function handleResponse(response) {
+	if (this.readyState == 4 && this.status == 200) {
+        var movie = JSON.parse(this.responseText);
+		if (movie == 0) {
+			showError();
+		} else {
+			renderHtml(movie);
+		}
+    }
+}
+
+function renderHtml(data) {
+	alert(data.Title + ' ' + data.Year);
+}
+
+function showError() {
+	alert('Error');
+}
